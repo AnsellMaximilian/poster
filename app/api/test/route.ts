4531476162b8cc85cb5e3009018ea7e1970f9f16
Ansell Maximilian <ansellmaximilian@gmail.com>
2025-05-30 10:53:42 +0700

    add logs

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -3,6 +3,8 @@ import { NextRequest, NextResponse } from "next/server";
 const POST = async (req: NextRequest) => {
   const body = await req.json();
 
+  console.log({ body });
+
   return NextResponse.json({
     body,
   });
